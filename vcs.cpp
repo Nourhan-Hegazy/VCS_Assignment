bba0a75ce8a87682d7cd8bb1593cce5144e87331
Nourhan-Hegazy <83380553+Nourhan-Hegazy@users.noreply.github.com>
2023-11-15 22:50:03 +0200

    getMin function

--- vcs.cpp
+++ vcs.cpp
@@ -1,15 +1,6 @@
 #include <vector>
 #include <iostream>
 
-int getSum(const std::vector<int>& vec) {
-    int sum = 0;
-
-    for (int element : vec) {
-        sum += element;
-    }
-
-    return sum;
-}
 
 int getMin(const std::vector<int>& vec){
     int min = vec[0];
@@ -26,10 +17,8 @@ int getMin(const std::vector<int>& vec){
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 0, 6, 7, 8, 9, 10};
 
-    int sum = getSum(numbers);
     int min = getMin(numbers);
 
-    std::cout << "Sum of the vector elements: " << sum << std::endl;
     std::cout << "Minimum of the vector elements: " << min << std::endl;
 
     return 0;
