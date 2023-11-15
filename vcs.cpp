74d95e46a458ed08c87dd07ce3a434b8ea3d2256
Nourhan-Hegazy <83380553+Nourhan-Hegazy@users.noreply.github.com>
2023-11-15 15:45:59 +0200

    Added getAverage function

--- vcs.cpp
+++ vcs.cpp
@@ -11,13 +11,26 @@ int getSum(const std::vector<int>& vec) {
     return sum;
 }
 
+float getAverage(const std::vector<int>& vec){
+    int length = 0;
+    for (int element : vec) {
+        length += 1;
+    }
+    int sum = getSum(vec);
+    float average = sum/length;
+    
+    return average;
+}
+
 
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    int result = getSum(numbers);
+    int sum = getSum(numbers);
+    float average = getAverage(numbers);
 
-    std::cout << "Sum of the vector elements: " << result << std::endl;
+    std::cout << "Sum of the vector elements: " << sum << std::endl;
+    std::cout << "Average of the vector elements: " << average << std::endl;
 
     return 0;
 }
